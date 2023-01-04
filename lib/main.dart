10a073132645a902261fb69c4732c72a7192eeaf
Arxyma <ilham.arsyam@gmail.com>
2023-01-04 11:17:45 +0700

    main edit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food recipe',
+      title: 'Resep Kita',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
